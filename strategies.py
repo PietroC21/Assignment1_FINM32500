dbc16f9325900750a14ca258bdfd6b8ffcabff9a
PietroC21 <pietrocandiani@Pietros-MacBook-Air.local>
2025-09-17 22:00:40 -0500

    Updated file with the two strategies for backtest

--- strategies.py
+++ strategies.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections import deque
 from data_generator import MarketDataPoint
 
 class Strategy(ABC):
@@ -6,6 +7,65 @@ class Strategy(ABC):
     def generate_signals(self, tick: MarketDataPoint) -> list:
         pass
 
-class MovingAverage(Strategy):
-    def generate_signals(self, tick):
+class MovingAverageStrategy(Strategy):
+    """
+    Buy when short moving average crosses above long moving average
+    Sell when short moving average crosses below long moving average
+    """
+    def __init__(self, quantity, short_window = 5, long_window = 10):
+        if short_window>= long_window:
+            raise ValueError("Short Window must be smaller than Long Window")
+
+        self.__short_window = short_window
+        self.__long_window = long_window
+        self.__prices =  deque(maxlen=long_window)
+        self._quantity = quantity
+        self.__last_signal = None       #BUY or SELL signal or None
+
+    def __moving_average(self, prices, window):
+        if len(prices)<window:
+            return None
+        return sum(list(prices)[-window:])/window
+    
+    def generate_signals(self, tick: MarketDataPoint)-> list:
+        signals = []
+        self.__prices.append(tick.price)
+
+        short_ma = self.__moving_average(self.__prices, self.__short_window)
+        long_ma = self.__moving_average(self.__prices, self.__long_window)
+
+        if short_ma and long_ma:
+            if short_ma>long_ma and self.__last_signal!='BUY':
+                signals.append((("BUY", tick.symbol,self._quantity, tick.price )))
+                self.__last_signal = "BUY"
+            elif short_ma<long_ma and self.__last_signal!="SELL":
+                 signals.append((("SELL", tick.symbol,self._quantity, tick.price )))
+                 self.__last_signal = "SELL"
+        return signals
+        
+
+class MomentumStrategy(Strategy):
+    """ If the last N prices are increasing, BUY
+        If the last N prices are decreasing SELL """
+    def __init__(self, quantity, lookback= 5):
+        self.__lookback = lookback
+        self.__quantity = quantity
+        self.__prices = deque(maxlen=lookback)
+
+    def generate_signals(self, tick : MarketDataPoint):
+        signals = []
+        self.__prices.append(tick.price)
+
+        if len(self.__prices) == self.__lookback:
+            
+            increasing = all(self.__prices[i]<=self.__prices[i+1] for i in range(len(self.__prices)-1))
+            decreasing = all(self.__prices[i]>=self.__prices[i+1] for i in range(len(self.__prices)-1))
+
+            if increasing:
+                signals.append((("BUY", tick.symbol,self._quantity, tick.price )))
+            elif decreasing:
+                signals.append((("SELL", tick.symbol,self._quantity, tick.price )))
+        return signals
+        
+        
         
